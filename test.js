3e1179d86ae61ee211db0d78f841b39d8f1f3007
Alvaro Lacouture <lacouture100@gmail.com>
2020-03-05 13:23:32 -0500

    Added oled screen functionality

--- test.js
+++ test.js
@@ -7,7 +7,10 @@ const fs = require("fs");
 const path = require("path");
 const moment = require("moment");
 
-
+const i2c = require('i2c-bus');
+const i2cBus = i2c.openSync(1);
+const screen = require('oled-i2c-bus');
+const font = require('oled-font-5x7');
 
 
 
@@ -58,7 +61,7 @@ let sensorReadings = {}; // object for device characteristics]
 
 }  */
 
-
+//Read the temperature and humidity from the DHT11 sensor
 function readSensorDataDHT11() {
    {
 
@@ -73,10 +76,12 @@ function readSensorDataDHT11() {
                `temperature: ${sensorReadings.temperature.toFixed(1)}°C, `,
                `humidity: ${sensorReadings.humidity.toFixed(1)}%`
             );
-            
+            //log Sensor data
+            logSensorData(sensorReadings);
             //send message to server
             sendToServer(JSON.stringify(sensorReadings));
-            logSensorData(sensorReadings);
+            displaySensorData(sensorReadings)
+
             clearInterval(readingInterval);
             return sensorReadings;
          };
@@ -87,7 +92,7 @@ function readSensorDataDHT11() {
    }
 }
 
-
+//Server response callback
 function getServerResponse(response) {
    // when the final chunk comes in, print it out:
    response.on('end', function (data) {
@@ -95,15 +100,13 @@ function getServerResponse(response) {
    });
 }
 
-
-
 // assemble the HTTPS request and send it:
-function sendToServer(dataToSend) {
+function sendToServer(data) {
    // make the POST data a JSON object and stringify it:
    var postData = JSON.stringify({
       'macAddress': /*'dc:a6:32:1f:5b:5f',*/ 'AA:BB:CC:DD:EE:FF',
       'sessionKey': '12345678',
-      'data': dataToSend
+      'data': data
    });
 
    /*
@@ -124,27 +127,57 @@ function sendToServer(dataToSend) {
 
    var request = https.request(options, getServerResponse); // start it
    request.write(postData); // send the data
-   //console.log(postData);
    request.end(); // end it
    console.log("Success! Message sent.")
 
 }
 
+//Log sensor data into the datalog/data.txt file
 function logSensorData(data) {
    let logTime = moment().format() // 2020-03-05T09:23:03-05:00
    logTime = logTime.toString().slice(0, -6);
    logTime = logTime.replace("T", "_");
 
    data = JSON.stringify(data);
-   //data = data.replace('{"temperature":', 'T');
-   //data = data.replace('}', '-');
 
    let datalog = logTime + data;
-   //fs.writeFileSync(path.join(__dirname, '/datalog/data.txt'), datalog);
+   //Write every new log into the data.txt file with timestamp
    fs.appendFile(path.join(__dirname, '/datalog/data.txt'), `\n${datalog}`, (err) => {
       if (err) throw err;
       console.log(`The datalog was updated at ${logTime}`);
-  });
+   });
+}
 
+function displaySensorData(data){
+   var opts = {
+      width: 128,     // screen width and height
+      height: 64,
+      address: 0x3C  // I2C address:check your particular model
+   };
+   var oled = new screen(i2cBus, opts);
+   // clear the screen:
+   oled.clearDisplay();
+
+   // generate new datetime object:
+   let logTime = moment().format() // 2020-03-05T09:23:03-05:00
+   logTime = logTime.toString().slice(0, -6);
+   logTime = logTime.replace("T", "_");
+
+   data = JSON.stringify(data);
+   let datalog = logTime + data;
+
+   let lastSent = moment().startOf('hour').fromNow();  // 24 minutes ago
+
+   // set cursor to x = 0 y = 0:
+   oled.setCursor(0, 0);
+   oled.writeString(font, 1, datalog, 1, true);
+
+      // set cursor to x = 0 y = 0:
+      oled.setCursor(0, 10);
+      oled.writeString(font, 1, lastSent, 1, true);
+}
+
+// update once per second:
+setInterval(showTime, 1000);
 }
 readingInterval = setInterval(readSensorDataDHT11, 1000);

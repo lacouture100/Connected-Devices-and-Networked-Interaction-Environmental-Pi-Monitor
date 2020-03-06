7272a6d1993ffd0e401666ebcc26484c7d5f4165
Alvaro Lacouture <lacouture100@gmail.com>
2020-03-05 19:02:55 -0500

    Added screen log separate

--- display.js
+++ display.js
@@ -10,58 +10,58 @@ const font = require('oled-font-5x7');
  */
 try {
 
-fs.readFile(path.join(__dirname, '/datalog/data.txt'), (err, data) => {
-  if (err) {
-    console.error(err)
-    return
-  }
-  //console.log(data)
+    fs.readFile(path.join(__dirname, '/datalog/data.txt'), (err, data) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        //console.log(data)
 
-/*   let tempData = data.substring(data.indexOf('t'), data.indexOf('\n'));
-  let humidData = data.substring(data.indexOf('\n'), -1); */
+        /*   let tempData = data.substring(data.indexOf('t'), data.indexOf('\n'));
+          let humidData = data.substring(data.indexOf('\n'), -1); */
 
-console.log(data)
-  //console.log(`${tempData}C\n${humidData}%`)
-displayTimeSinceSent();
-  displaySensorData(data);
+        console.log(data)
+        //console.log(`${tempData}C\n${humidData}%`)
+        displayTimeSinceSent();
+        displaySensorData(data);
 
-})
+    })
+}
 
 
 
+    /* let logTime = moment().format();
+    console.log(logTime);
+    logTime = logTime.toString().slice(0,-6);
+    logTime = logTime.replace("T", "_");
+    console.log(logTime); */
+    //
+    function displaySensorData(data) {
+        // generate new datetime object:
+        //let logTime = moment().format() // 2020-03-05T09:23:03-05:00
+        //logTime = logTime.toString().slice(0, -6);
+        //logTime = logTime.replace("T", "_");
+        data = data.toString().substr(-60, 60);
+        /*   data = data.replace('{"','');
+          data = data.replace(/"/g,''); 
+          data = data.replace(',','\n');
+          data = data.replace('}',''); */
+        data = data.replace(/"/g, '');
 
-/* let logTime = moment().format();
-console.log(logTime);
-logTime = logTime.toString().slice(0,-6);
-logTime = logTime.replace("T", "_");
-console.log(logTime); */
-//
-function displaySensorData(data) {
-    // generate new datetime object:
-    //let logTime = moment().format() // 2020-03-05T09:23:03-05:00
-    //logTime = logTime.toString().slice(0, -6);
-    //logTime = logTime.replace("T", "_");
-    data=data.toString().substr(-60,60);
-    /*   data = data.replace('{"','');
-      data = data.replace(/"/g,''); 
-      data = data.replace(',','\n');
-      data = data.replace('}',''); */
-      data = data.replace(/"/g,''); 
-    
-      data = data.substring(data.indexOf('t'), data.indexOf('}'));
-      data = data.replace(',','\n');
-    // set cursor to x = 0 y = 0:
-    oled.setCursor(0, 30);
-    oled.writeString(font, 1, data, 1, true);
- }
- 
- function displayTimeSinceSent() {
- 
-    //let lastSent = moment().startOf('hour').fromNow(); // 24 minutes ago
-    let now = new Date();
-    let lastSent= (now.getMinutes()).toString()
-    // set cursor to x = 0 y = 0:
-    oled.setCursor(0, 0);
-    oled.writeString(font, 1, `Last message sent: \n${lastSent} minutes ago`, 1, true);
- 
- }  
+        data = data.substring(data.indexOf('t'), data.indexOf('}'));
+        data = data.replace(',', '\n');
+        // set cursor to x = 0 y = 0:
+        oled.setCursor(0, 30);
+        oled.writeString(font, 1, data, 1, true);
+    }
+
+    function displayTimeSinceSent() {
+
+        //let lastSent = moment().startOf('hour').fromNow(); // 24 minutes ago
+        let now = new Date();
+        let lastSent = (now.getMinutes()).toString()
+        // set cursor to x = 0 y = 0:
+        oled.setCursor(0, 0);
+        oled.writeString(font, 1, `Last message sent: \n${lastSent} minutes ago`, 1, true);
+
+    }

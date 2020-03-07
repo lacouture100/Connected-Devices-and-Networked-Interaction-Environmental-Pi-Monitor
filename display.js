cb6474256b05e7e5d6a2646a98e72fc2f0dbe0b7
Alvaro Lacouture <lacouture100@gmail.com>
2020-03-07 12:39:40 -0500

    updated wwith cron node package

--- display.js
+++ display.js
@@ -8,17 +8,19 @@ const i2cBus = i2c.openSync(1);
 const screen = require('oled-i2c-bus');
 const font = require('oled-font-5x7');
 
+let cron = require('node-cron');
 
-var opts = {
+let opts = {
     width: 128, // screen width and height
     height: 64,
     address: 0x3C // I2C address:check your particular model
  };
- var oled = new screen(i2cBus, opts);
+let oled = new screen(i2cBus, opts);
 
- oled.clearDisplay();
-
-fs.readFile(path.join(__dirname, '/datalog/data.txt'), (err, data) => {
+let cronTask = cron.schedule('5 * * * * *', () =>  {
+  console.log('stopped task');
+  oled.clearDisplay();
+  fs.readFile(path.join(__dirname, '/datalog/data.txt'), (err, data) => {
     if (err) {
         console.error(err)
         return
@@ -30,6 +32,12 @@ fs.readFile(path.join(__dirname, '/datalog/data.txt'), (err, data) => {
     displaySensorData(data);
 
 })
+}, {
+  scheduled: false
+});
+
+cronTask.start();
+
 function displaySensorData(data) {
     // generate new datetime object:
     //let logTime = moment().format() // 2020-03-05T09:23:03-05:00
@@ -45,7 +53,6 @@ function displaySensorData(data) {
     // set cursor to x = 0 y = 0:
     oled.setCursor(0, 0);
     oled.writeString(font, 1, data, 1, true);
-    
 }
 
 function displayTimeSinceSent() {
@@ -56,5 +63,4 @@ function displayTimeSinceSent() {
     // set cursor to x = 0 y = 0:
     oled.setCursor(0, 30);
     oled.writeString(font, 1, `Last message sent: \n${lastSent} minutes ago`, 1, true);
-    
 }
